90769d27716f01b31a981af9b470fa33e586e242
Pete Nicholls <pete@metanation.com>
2013-10-05 14:15:35 +1300

    Use Express, capture last tweet in Redis

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
 var util = require('util'),
     twitter = require('twitter'),
-    http = require('http'),
-    url = require("url"),
-    path = require("path"),
-    fs = require("fs");
-
-// App settings
-var account = process.env.TWITTER_ACCOUNT,
-    port = process.env.PORT,
-    public_dir = 'public';
+    url = require('url'),
+    ejs = require('ejs'),
+    express = require("express");
 
 // Persistence
 var redis;
@@ -25,6 +19,8 @@ redis.on("error", function (err) {
 });
 
 // Twitter
+var twitterAccount = process.env.TWITTER_ACCOUNT;
+
 var twit = new twitter({
     consumer_key: process.env.CONSUMER_KEY,
     consumer_secret: process.env.CONSUMER_SECRET,
@@ -32,43 +28,29 @@ var twit = new twitter({
     access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
-twit.stream('user', { track: account }, function(stream) {
+twit.stream('user', { track: twitterAccount }, function(stream) {
   stream.on('data', function(data) {
-    console.log(util.inspect(data));
+    redis.set('lastTweet', JSON.stringify(data));
   });
 });
 
-// Simple HTTP server
-http.createServer(function (request, response) {
-  var uri = url.parse(request.url).pathname,
-      filename = path.join([process.cwd(), public_dir].join('/'), uri);
-  
-  fs.exists(filename, function(exists) {
-    if(!exists) {
-      response.writeHead(404, {"Content-Type": "text/plain"});
-      response.write("404 Not Found\n");
-      response.end();
-      return;
-    }
- 
-    if (fs.statSync(filename).isDirectory()) {
-      filename += '/index.html';
-    }
- 
-    fs.readFile(filename, "binary", function(err, file) {
-      if(err) {        
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(err + "\n");
-        response.end();
-        return;
-      }
- 
-      response.writeHead(200);
-      response.write(file, "binary");
-      response.end();
-    });
-  });
+// HTTP
+var port = process.env.PORT || 5000;
 
-}).listen(port);
+var app = express();
+app.engine('html', ejs.renderFile);
+app.use(express.logger());
+app.use(express.static(__dirname + '/public'));
 
-console.log('Server running on port', port);
+app.get('/', function(request, response) {
+  var locals = {
+    lastTweet: redis.get('lastTweet'),
+    inspect: util.inspect
+  };
+
+  response.render('index.ejs', locals);
+});
+
+app.listen(port, function() {
+  console.log('Listening on ' + port);
+});
